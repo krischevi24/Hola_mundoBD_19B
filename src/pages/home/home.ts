96eee416b48372a71eb398f0203d9a2aa05c8feb
Kevin <chevi24@live.com>
2019-10-16 20:12:08 -0500

    Agregamos datos de inicio y boton

---
+++ src/pages/home/home.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  nombre = '';
+
+  constructor(public navCtrl: NavController) {
+
+  }
+  click() {
+    console.log('Hola', this.nombre);
+  }
+
+}
